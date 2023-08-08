4f8d2f0aa091f9776a33f7a9d69ef748a756892b
Michou-G <michougalal.03@gmail.com>
2023-08-08 22:43:47 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles.css"
 
+
 // Main App Component
 export default function WheelApp() {
   const [segments, setSegments] = useState([]);
@@ -16,10 +17,18 @@ export default function WheelApp() {
   };
 
   const spinWheel = () => {
-    const spins = Math.floor(Math.random() * (10 - 3 + 1) + 3);
-    const newRotation = rotation + (360 * spins);
-    setRotation(newRotation);
-  };
+		// Random number of full spins (e.g., between 3 to 10 full spins)
+		const spins = Math.floor(Math.random() * (10 - 3 + 1) + 3);
+		
+		// Random segment position to ensure it stops at a different segment each time
+		const randomSegmentStop = 360 / segments.length * Math.floor(Math.random() * segments.length);
+		
+		// Calculate the new rotation value
+		const newRotation = rotation + (360 * spins) + randomSegmentStop;
+		
+		setRotation(newRotation);
+	};
+	
 
   return (
     <div className="app-container">
--- src/controlpanel.jsx
+++ src/controlpanel.jsx
@@ -1,8 +1,18 @@
 // Control Panel Component
 function ControlPanel({ onAddSegment, onRemoveSegment, onSpin }) {
+  const [label, setLabel] = useState('');
+  const [color, setColor] = useState('#ff0000');
+
+  const handleAdd = () => {
+    onAddSegment({ label, color });
+    setLabel('');
+  };
+
   return (
-    <div className="control-panel">
-      <button onClick={onAddSegment}>Add Segment</button>
+    <div className="controls">
+      <input value={label} onChange={e => setLabel(e.target.value)} placeholder="Segment Label" />
+      <input type="color" value={color} onChange={e => setColor(e.target.value)} />
+      <button onClick={handleAdd}>Add Segment</button>
       <button onClick={onRemoveSegment}>Remove Segment</button>
       <button onClick={onSpin}>Spin</button>
     </div>
