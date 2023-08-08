6de4862fa5887db4c019b3b66cdf9d00541639d3
Michou-G <michougalal.03@gmail.com>
2023-08-08 22:08:07 +0200

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./styles.css"
 
 export default function App() {
-  
+  const [rotation, setRotation] = useState(0);
+
+  const spinWheel = () => {
+    const spins = Math.floor(Math.random() * (10 - 3 + 1) + 3); // Random spins between 3 to 10
+    const newRotation = rotation + (360 * spins);
+    setRotation(newRotation);
+  };
 }
